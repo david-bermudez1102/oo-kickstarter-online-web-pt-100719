d93b0ce72c9351c7b588cf57fe77e773d2f6668c
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-20 00:05:26 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,12 @@
 class Backer
   attr_reader :name, :backed_projects
+
   def initialize(name, backed_projects=[])
     @name = name
     @backed_projects = backed_projects
+  end
+
+  def back_project(project)
+
   end
 end
